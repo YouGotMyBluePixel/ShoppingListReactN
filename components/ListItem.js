97e22505facd7231388b70f4a44de2c584fee510
YouGotMyBluePixel <49995801+YouGotMyBluePixel@users.noreply.github.com>
2020-10-25 19:38:48 -0400

    first commit

---
+++ components/ListItem.js
@@ -0,0 +1,41 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+// eslint-disable-next-line prettier/prettier
+const ListItem = ({item, deleteItem}) => {
+  return (
+    <TouchableOpacity style={styles.ListItem}>
+        <View style = {styles.ListItemView}>
+            <Text style = {styles.ListItemText}>{item.text} </Text>
+            <Icon
+             name="remove"
+             size={20}
+             color="firebrick"
+            onPress={()=> deleteItem(item.id)}
+            />
+    </View>
+    </TouchableOpacity>
+  );
+};
+
+
+const styles = StyleSheet.create({
+ListItem: {
+    padding: 15,
+    backgroundColor: '#f8f8f8',
+    borderBottomWidth:1,
+    borderColor: '#eee',
+},
+ListItemView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+},
+ListItemText: {
+    fontSize:18,
+
+},
+});
+
+export default ListItem;
